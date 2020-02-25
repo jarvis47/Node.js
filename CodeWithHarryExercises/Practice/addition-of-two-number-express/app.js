d01ae39cffa6d0468b253d40347005bcd9ce45b4
jarvis47 <47108332+jarvis47@users.noreply.github.com>
2020-02-25 12:02:11 +0530

    Add files via upload

---
+++ CodeWithHarryExercises/Practice/addition-of-two-number-express/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const fs = require("fs");
+const url = require("url");
+
+const app = express();
+const port = 80;
+
+const addNumberHTML = fs.readFileSync("./addNumber.html")
+
+app.get("/", (req, res) =>{
+
+    res.end(addNumberHTML)
+
+});
+
+app.get("/add", (req, res) =>{
+
+    const urlParsing = url.parse(req.url, true)
+    
+    res.send("<a href='\\'>Back</a><br><br> addition is : "+ (parseInt(urlParsing.query.num1)+parseInt(urlParsing.query.num2)));
+     
+});
+
+app.listen(port, ()=>{
+    console.log(`server is up and running on ${port}`);
+    
+});
